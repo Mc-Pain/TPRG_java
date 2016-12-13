57e75cce3514b409f755caedb0283c10f3754f5d
Mc-Pain <McPain2@mail.ru>
2016-12-13 17:49:21 +0300

    Revert "Revert "Vending Machine v0.1""
    This reverts commit 2e5fdb1549743e9882e43ef0cb0d9aafbd67306c.

---
+++ src/java/buyer.java
@@ -0,0 +1,19 @@
+import java.util.Vector;
+
+public class buyer { //ненужный класс
+	private int _money;
+	public Vector<product> _unnamed_product_ = new Vector<product>();
+	public vending_machine _unnamed_vending_machine_;
+
+	public void setmoney(int aMoney) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void buyproduct() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void paymoney(int aMoney) {
+		throw new UnsupportedOperationException();
+	}
+}
